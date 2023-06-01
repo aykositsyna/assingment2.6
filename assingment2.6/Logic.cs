044e704b83384ddc66eedd735442f79708dd0497
Alina <a.y.kositsyna@gmail.com>
2023-06-02 03:59:46 +0700

    copied riginas code. need to go through mistakes, ask questions, write code for "save" and "show" buttons

---
+++ assingment2.6/Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assingment2._6
+{
+    internal class Logic
+    {
+        public ObservableCollection<Dicipline> Subjects { get; set; }
+
+        public Dicipline SelectedSubject { get; set; }
+
+    }
+}
--- assingment2.6/MainWindow.xaml.cs
+++ assingment2.6/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Net.NetworkInformation;
 
 namespace assingment2._6
 {
@@ -20,14 +24,104 @@ namespace assingment2._6
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Logic logic;
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+            logic = new Logic();
+            logic.Subjects = new ObservableCollection<Dicipline>();
+
+            StartFileRead();
+            DataContext = logic;
+
+            RBtnAll.IsChecked = true;
         }
 
-        private void RBtnAll(object sender, RoutedEventArgs e)
+        public async Task Save()
+        {
+            string str = "";
+            foreach (Dicipline logicSubject in logic.Subjects)
+            {
+                if (str != "")
+                {
+                    str += "\n";
+                }
+
+                str += $"{logicSubject.Title},{logicSubject.Status}";
+            }
+            File.WriteAllText("subjects.txt", str);
+        }
+
+        public async Task StartFileRead()
+        {
+            var file = File.ReadAllText("subjects.txt");
+
+            var bn = file.Split('\n');
+            foreach (var s in bn)
+            {
+                var _array = s.Split(',');
+                logic.Subjects.Add(new Dicipline(_array[0], bool.Parse(_array[1])));
+            }
+        }
+        private void BtnChangeStat_Click(object sender, RoutedEventArgs e)
+        {
+            logic.SelectedSubject.CheckStatus();
+            Save();
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (logic.SelectedSubject != null)
+            {
+                logic.Subjects.Remove(logic.SelectedSubject);
+                Save();
+            }
+        }
+
+        private void RBtn_Checked(object sender, RoutedEventArgs e)
+        {
+            int index = 0;
+
+            if (RBtnPassed.IsChecked == true)
+            {
+                index = 1;
+            }
+
+            if (RBtnNotPassed.IsChecked == true)
+            {
+                index = 2;
+            }
+
+            foreach (Dicipline logicSubject in logic.Subjects)
+            {
+                logicSubject.SetFilter(index);
+            }
+
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void BtnShow_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBDicipName.Text != "" && CBDicipStatus.SelectedIndex != -1)
+            {
+                var Disc = new Dicipline(TBDicipName.Text, CBDicipStatus.SelectedIndex == 0);
+                logic.Subjects.Add(Disc);
+                TBDicipName.Text = "";
+                CBDicipStatus.SelectedIndex = -1;
+                Save();
+            }
         }
     }
 }
